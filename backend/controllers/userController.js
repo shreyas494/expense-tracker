23a81f5b797445b1df8fb55115cb5dc7d5c39751
shreyas494 <shreyakulkarni850@gmail.com>
2026-07-08 16:19:45 +0530

    Allow JWT_SECRET to be configured dynamically via environment variables

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ import validator from "validator";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = 'your_jwt_secret_here';
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_here';
 const TOKEN_EXPIRES = '24h';
 
 const createToken = (userId) => 
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,7 @@
 import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = 'your_jwt_secret_here';
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_here';
 
 export default async function authMiddleware(req,res,next){
     const authHeader = req.headers.authorization;
