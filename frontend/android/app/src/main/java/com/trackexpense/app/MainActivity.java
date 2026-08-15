b4f4ac80f8f1b3777a7c9ea851be0529c983f197
shreyas494 <shreyakulkarni850@gmail.com>
2026-08-15 22:50:57 +0530

    fix(android/java): change onResume and onNewIntent to public access modifier in MainActivity.java to fix compiler override error

--- frontend/android/app/src/main/java/com/trackexpense/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/trackexpense/app/MainActivity.java
@@ -30,13 +30,13 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         checkAndStartService();
     }
 
     @Override
-    protected void onNewIntent(Intent intent) {
+    public void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         checkAndStartService();
     }
